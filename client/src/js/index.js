158cdcd7d395a306637ce427b85eaaf3514830cb
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-05 02:44:03 -0500

    added webpack for js, imgs, css, and plugins

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,19 @@
+// import modules
 import "./form";
 import "./submit";
+
+// import CSS
+import "../css/index.css";
+import { Tooltip, Toast, Popoever } from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+import Sam from '../images/sam-avatar.png';
+
+window.addEventListener('load', function() {
+  document.getElementById('logo').src = Sam;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
