f954513cd6354d97fc2d0313be51df5092c4f45e
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-07 20:19:39 -0500

    added IndexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -7,12 +7,18 @@ import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import Sam from '../images/sam-avatar.png';
 
+// import db
+import { initdb  } from "./database";
+
+
 window.addEventListener('load', function() {
+  initdb();
   document.getElementById('logo').src = Sam;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
